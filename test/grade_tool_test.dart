75c6bbf904f878b977b21cfcb4d89214cc8f8e3f
nbrat <nbratek99@gmail.com>
2024-08-31 16:11:46 -0500

    Add tests for each case

--- test/grade_tool_test.dart
+++ test/grade_tool_test.dart
@@ -7,4 +7,20 @@ void main() {
     final result = gradeTool.assignLetterGrade(56.4);
     expect(result, "F");
   });
+  test('67.5 is a D', () {
+    final result = gradeTool.assignLetterGrade(67.5);
+    expect(result, "D");
+  });
+  test('79.9 is a C', () {
+    final result = gradeTool.assignLetterGrade(79.9);
+    expect(result, "C");
+  });
+  test('80.0 is a B', () {
+    final result = gradeTool.assignLetterGrade(80.0);
+    expect(result, "B");
+  });
+  test('94.0 is an A', () {
+    final result = gradeTool.assignLetterGrade(94.0);
+    expect(result, "A");
+  });
 }
